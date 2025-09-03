8b0ecfb3006a8f87858e87bdfe439f28407447af
Geovana <marketing@letscode.com.br>
2025-09-03 17:00:08 -0300

    Adição de um noco produto na tabela produtos

--- script.sql
+++ script.sql
@@ -16,10 +16,12 @@ CREATE TABLE pedidos (
 INSERT INTO produtos (id, nome, preco, estoque) VALUES
 (1, 'Café Expresso', 12.50, 30),
 (2, 'Capuccino', 15.00, 20),
-(3, 'Bolo de Cenoura', 8.00, 10);
+(3, 'Bolo de Cenoura', 8.00, 5;
+(4, 'suco de laranja', 10.00, 10);
 
 INSERT INTO pedidos (id, produto_id, quantidade, data_pedido) VALUES
 (1, 1, 2, '2025-09-01'),
 (2, 3, 1, '2025-09-02'),
 (3, 2, 3, '2025-09-02');
---script atualizado em 03/09
+(4, 4,) 2, '2025-09-03'),
+--adição do suco de laranja nos produtos
